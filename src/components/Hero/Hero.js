5b0c49265a3fbc87a521a36eeba355050bfca65d
Manvendra Singh <mjdeora@gmail.com>
2018-07-19 17:16:27 +0530

    hero flex refactored

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -2,12 +2,10 @@ import React from 'react';
 import './Hero.css';
 
 export default () => (
-  <div className="container">
-    <div className="section">
-      <div className="time text">16:30</div>
-      <div className="greeting text">Good afternoon, Madar</div>
-      <div className="question text">What is your main focus for today?</div>
-      <input type="text" className="focus" />
-    </div>
+  <div className="section">
+    <div className="time">16:30</div>
+    <div className="greeting">Good afternoon, Manven</div>
+    <div className="question">What is your main focus for today?</div>
+    <input type="text" className="focus" />
   </div>
 );
